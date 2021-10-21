b6b459e13b6a0e0228f4121e14e93cc662b03750
Chloe Harris <chloe.a.harris17@gmail.com>
2021-10-20 17:43:53 -0700

    Changed duplicate index.js to server.js. Changed notesRoutes file name to notes.js

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const PORT = process.env.PORT ||3000;
 
 const htmlRoutes = require("./routes/htmlRoutes");
 
-// First route
+// GET Route for homepage
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
+app.use(express.static('public'));
 
 // Listen for requests
 app.listen( PORT, ()=>{
